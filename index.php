81b7b6f4834ff4e360dc6b905a8897e5189a1ec7
JN646 <josh.ginn@hotmail.co.uk>
2018-08-31 11:38:56 +0100

    Init.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>The Crimes</h1>
+      <p>Are you at risk of attack?</p>
+      <form class="" action="" method="post">
+        <label for="">Longitude</label>
+        <input type="text" name="long" value="">
+        <label for="">Latitude</label>
+        <input type="text" name="lat" value="">
+        <button type="submit" name="button">Search</button>
+      </form>
+
+      <p style="color: red;">You are at risk!</p>
+    </div>
+  </body>
+</html>
