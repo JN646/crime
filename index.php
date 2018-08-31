378517b816af13cbbde9de077de132d05becfee3
JN646 <josh.ginn@hotmail.co.uk>
2018-08-31 16:48:21 +0100

    Conflict Repair.

--- index.php
+++ index.php
@@ -33,18 +33,11 @@
       if (isset($_POST["btnSearch"])) {
           $longVal = $_POST["long"];
           $latVal = $_POST["lat"];
-<<<<<<< HEAD
           $radVal = $_POST["rad"];
 
           // Get SQL
           $sql = "SELECT id, Longitude, Latitude, Crime_Type FROM data WHERE
           SQRT(POW(Latitude-'$latVal', 2)+POW(Longitude-'$longVal', 2))<'$radVal' ORDER BY Crime_Type ASC";
-=======
-
-          // Get SQL
-          $sql = "SELECT id, Longitude, Latitude, Crime_Type FROM data WHERE Longitude='$longVal' OR Latitude='$latVal'  ORDER BY Crime_Type ASC";
->>>>>>> 4c0501795631da1da0c8108dd633eb616e815c73
-          $result = mysqli_query($mysqli, $sql);
 
           // Fetch Results
           if (mysqli_num_rows($result) > 0) {
