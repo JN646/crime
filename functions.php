a0b1402b0e44ecc8b93ea6517a9752d335ff5768
JN646 <josh.ginn@hotmail.co.uk>
2018-08-31 16:06:07 +0100

    CSS and basic search.

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+//MySQL connection
+$mysqli = new mysqli('localhost', 'root', '', 'crimes');
+
+ //If connection fail
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+ ?>
--- index.php
+++ index.php
@@ -2,21 +2,63 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Crime App</title>
+    <link rel="stylesheet" href="css/master.css">
+    <?php include 'functions.php' ?>
   </head>
   <body>
+    <?php
+    // Debug
+    $safety = "safe";
+     ?>
+    <!-- Container -->
     <div class="container">
       <h1>The Crimes</h1>
       <p>Are you at risk of attack?</p>
+
+      <!-- Search Form -->
       <form class="" action="" method="post">
         <label for="">Longitude</label>
         <input type="text" name="long" value="">
         <label for="">Latitude</label>
         <input type="text" name="lat" value="">
-        <button type="submit" name="button">Search</button>
+        <button type="submit" name="btnSearch">Search</button>
       </form>
 
-      <p style="color: red;">You are at risk!</p>
+      <!-- Risk Notification -->
+      <?php if ($safety == "safe") { ?>
+        <p class="safe">You might be safe!</p>
+      <?php } elseif ($safety == "danger") { ?>
+        <p class="danger">You are at risk!</p>
+      <?php } ?>
+
+      <?php
+      if (isset($_POST["btnSearch"])) {
+          $longVal = $_POST["long"];
+
+          // Get SQL
+          $sql = "SELECT id, Longitude, Latitude, Crime_Type FROM data WHERE Longitude='$longVal' ORDER BY Crime_Type ASC";
+          $result = mysqli_query($mysqli, $sql);
+
+          // Fetch Results
+          if (mysqli_num_rows($result) > 0) {
+              // output data of each row
+              while ($row = mysqli_fetch_assoc($result)) {
+                  // Set Variables
+                  $id = $row["id"];
+                  $long = $row["Longitude"];
+                  $lat = $row["Latitude"];
+                  $crime_type = $row["Crime_Type"];
+
+                  echo "<p class='outputText'><b>Long:</b> " . $long . " <b>Lat:</b> " . $lat . " <b>Crime Type:</b> " . $crime_type . "</p>";
+              }
+          } else {
+              // No Results
+              echo "0 results";
+          }
+      }
+
+       ?>
     </div>
   </body>
 </html>
