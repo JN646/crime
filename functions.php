d2c88fab8c9de52c1955f36c86f53fa174a32b7b
JN646 <josh.ginn@hotmail.co.uk>
2018-09-01 21:00:55 +0100

    Added Local and Immediate filters.

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-//############## FUNCTION FILE #################################################
+//############## FUNCTION FILE #######################################################
 //MySQL connection
 $mysqli = new mysqli('localhost', 'root', '', 'crimes');
 
@@ -8,7 +8,28 @@ if ($mysqli->connect_error) {
   die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 
-//############## RISK MATRIX ###################################################
+//############## RUN MAP #############################################################
+function getMap($latVal, $longVal) {
+  ?>
+  <div id="map" style="width:100%;height:300px"></div>
+
+  <script>
+  function myMap() {
+    var myCenter = new google.maps.LatLng(<?php echo $latVal ?>,<?php echo $longVal ?>);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {center: myCenter, zoom: 15};
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
+  }
+  </script>
+
+  <!-- Map API Key -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDntrXRGpts74HjwJQbirjHqKW_Cq50lSU&callback=myMap"></script>
+  <?php
+}
+
+//############## RISK MATRIX #########################################################
 function getRisk($crime_count) {
   if ($crime_count >= 5) {
     $crime_risk = "High";
--- index.php
+++ index.php
@@ -16,9 +16,11 @@
     <?php
     // Debug
     $safety = "safe";
-    $rad = 0;
     $monthVal = "January";
     $yearVal = "2018";
+    $radVal1 = 0;
+    $radVal2 = 0;
+    $n = 0;
      ?>
     <!-- Container -->
     <div class="container">
@@ -31,24 +33,19 @@
         <input size='8' type="text" name="lat" value="52.122123">
         <label for="">Longitude</label>
         <input size='8' type="text" name="long" value="-0.586406">
-        <label for="">Radius</label>
-        <input type="range" style="vertical-align: middle;" min="0.01" max="1.00" value="0.05" step="0.01" class="slider" id="myRange">
-        <input id='radius' size='3' type="text" name="rad" value="0.08">
+        <label for="">immediate area</label>
+        <input id='radius' size='3' type="text" name="rad1" value="0.1">
+        <label for="">local area</label>
+        <input id='radius2' size='3' type="text" name="rad2" value="0.4">
         <label for="">Month</label>
         <select class="" name="month">
-          <option selected='selected' value='January'>January</option>
-          <option value="Feburary">Feburary</option>
-          <option value="March">March</option>
-          <option value="April">April</option>
-          <option value="May">May</option>
-          <option value="June">June</option>
-          <option value="July">July</option>
-          <option value="August">August</option>
-          <option value="September">September</option>
-          <option value="October">October</option>
-          <option value="November">November</option>
-          <option value="December">December</option>
-        </select>
+          <?php $monthVariables = ["January","Feburary","March","April","May","June","July","August","September","October","November","December"];
+            for ($i=0; $i < count($monthVariables) ; $i++) {
+                ?>
+              <option value="<?php echo $monthVariables[$i] ?>"><?php echo $monthVariables[$i] ?></option>
+          <?php
+            } ?>
+         </select>
         <label for="">Year</label>
         <select class="" name="year">
           <option value='2016'>2016</option>
@@ -57,112 +54,121 @@
         </select>
         <label for="">Case</label>
         <select class="" name="case">
-          <option value='0'>0</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
+          <?php for ($i=0; $i < 3; $i++) {
+                echo "<option value='" . $i . "'>" . $i . "</option>";
+            } ?>
         </select>
 
+        <!-- Search Button -->
         <button type="submit" name="btnSearch"><i class="fas fa-search"></i></button>
       </form>
-
-
 
       <?php
       // Search Button Press
       if (isset($_POST["btnSearch"])) {
           $longVal = trim($_POST["long"]);
           $latVal = trim($_POST["lat"]);
-          $radVal = trim($_POST["rad"]);
+          $radVal1 = trim($_POST["rad1"]);
+          $radVal2 = trim($_POST["rad2"]);
           $monthVal = trim($_POST["month"]);
           $yearVal = trim($_POST["year"]);
           $searchCase = trim($_POST["case"]);
 
           // Get SQL
           //precalculation of ranges
-          $latLow = $latVal - $radVal;
-          $latHigh = $latVal + $radVal;
-          $longLow = $longVal - $radVal;
-          $longHigh = $longVal + $radVal;
+          $latLow1 = $latVal - $radVal1;
+          $latHigh1 = $latVal + $radVal1;
+          $longLow1 = $longVal - $radVal1;
+          $longHigh1 = $longVal + $radVal1;
 
-          $starttime = microtime(true);
-          $sql = "SELECT COUNT(id), Longitude, Latitude, Crime_Type, Month, Year FROM data
-          WHERE Longitude > $longLow AND Longitude < $longHigh and Latitude > $latLow AND Latitude < $latHigh AND SQRT(POW(Latitude-'$latVal', 2)+POW(Longitude-'$longVal', 2))<'$radVal'
+          $latLow2 = $latVal - $radVal2;
+          $latHigh2 = $latVal + $radVal2;
+          $longLow2 = $longVal - $radVal2;
+          $longHigh2 = $longVal + $radVal2;
+
+          $starttime = microtime(true); // Start Timer
+
+          //immediate area
+          $sql1 = "SELECT COUNT(id), Longitude, Latitude, Crime_Type, Month, Year FROM data
+          WHERE Longitude > $longLow1 AND Longitude < $longHigh1 and Latitude > $latLow1 AND Latitude < $latHigh1 AND SQRT(POW(Latitude-'$latVal', 2)+POW(Longitude-'$longVal', 2))<'$radVal1'
           AND Month='$monthVal'
           AND Year='$yearVal'
           GROUP BY Crime_Type
           ORDER BY COUNT(id) DESC";
 
-          // Run Result
-          $resultCount = mysqli_query($mysqli, $sql);
-          if (!$resultCount) {
-            die('Could not run query: ' . mysqli_error($mysqli));
+          //local area
+          $sql2 = "SELECT COUNT(id), Longitude, Latitude, Crime_Type, Month, Year FROM data
+          WHERE Longitude > $longLow2 AND Longitude < $longHigh2 and Latitude > $latLow2 AND Latitude < $latHigh2 AND SQRT(POW(Latitude-'$latVal', 2)+POW(Longitude-'$longVal', 2))<'$radVal2'
+          AND Month='$monthVal'
+          AND Year='$yearVal'
+          GROUP BY Crime_Type
+          ORDER BY COUNT(id) DESC";
+
+          // Run Results
+          $resultCount1 = mysqli_query($mysqli, $sql1);
+          $resultCount2 = mysqli_query($mysqli, $sql2);
+
+          // If Error
+          if (!$resultCount1 || !$resultCount2) {
+              die('Could not run query: ' . mysqli_error($mysqli));
           }
 
-          $duration = microtime(true) - $starttime; //calculates total time taken
+          $duration = microtime(true) - $starttime; // Calculates total time taken
+
+          // Get Map
+          getMap($latVal, $longVal);
 
           // Fetch Results
-          if (mysqli_num_rows($resultCount) > 0) {
+          if (mysqli_num_rows($resultCount1) > 0 || mysql_num_rows($resultCount2) > 0) {
               ?>
-            <div id="map" style="width:100%;height:300px"></div>
-
-            <script>
-            function myMap() {
-              var myCenter = new google.maps.LatLng(<?php echo $latVal ?>,<?php echo $longVal ?>);
-              var mapCanvas = document.getElementById("map");
-              var mapOptions = {center: myCenter, zoom: 15};
-              var map = new google.maps.Map(mapCanvas, mapOptions);
-              var marker = new google.maps.Marker({position:myCenter});
-              marker.setMap(map);
-            }
-            </script>
-
-            <!-- Map API Key -->
-            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDntrXRGpts74HjwJQbirjHqKW_Cq50lSU&callback=myMap"></script>
 
               <!-- Result Table -->
+              <h2>Crimes Around You</h2>
               <table class='table-border' width=100%>
-				        <tr>
-              		<th class='text-center text-bold'>Crime</th>
-              		<th class='text-center text-bold'>Count</th>
-              		<th class='text-center text-bold'>Risk</th>
-              	</tr>
+                <tr>
+                  <th class='text-center text-bold'>Crime</th>
+                  <th class='text-center text-bold'>Immediate</th>
+                  <th class='text-center text-bold'>Local</th>
+                  <th class='text-center text-bold'>Risk</th>
+                </tr>
                 <?php
-              while ($row = mysqli_fetch_assoc($resultCount)) {
+
+              while ($row = mysqli_fetch_assoc($resultCount2)) {
                   // Set Variables
                   $crime_type = $row["Crime_Type"];
                   $crime_count = $row["COUNT(id)"]; ?>
                   <tr>
                     <td><?php echo $crime_type; ?></td>
-                    <td class='text-center'><?php echo $crime_count; ?></td>
+                    <td class='text-center'>
+                    <?php
+                    $n = 0;
+                    $row1 = mysqli_fetch_assoc($resultCount1);
+                    for ($i=0; $i < count($resultCount1); $i++) {
+                      if ($row1["Crime_Type"] == $crime_type) {
+                        $n = $row1["COUNT(id)"];
+                      }
+                    }
+                    echo $n;
+                     ?></td>
+                     <td class='text-center'><?php echo $crime_count; ?></td>
                     <td class='text-center'><?php echo "<span class=risk_" . getRisk($crime_count) .">" . getRisk($crime_count) . "</span>"?></td>
                   </tr>
-                  <?php
-              }
-              echo "</table>"; ?>
+                <?php } ?>
+              </table>
 
+              <hr>
+              <!-- Count Results -->
               <div id='resultStats'>
-                <hr>
-                <!-- Count Results -->
-                <p>Total: <?php echo mysqli_num_rows($resultCount) ?></p>
-                <p>Exec Time: <?php echo round($duration,4) ?></p>
+                <p>Total1: <?php echo mysqli_num_rows($resultCount1) ?></p>
+                <p>Total2: <?php echo mysqli_num_rows($resultCount2) ?></p>
+                <p>Exec Time: <?php echo round($duration, 4) ?></p>
               </div>
               <?php
           } else {
               // No Results
               echo "<p id='noResults'>0 results</p>";
           }
-      }
-       ?>
+      } ?>
     </div>
-    <script type="text/javascript">
-      var slider = document.getElementById("myRange");
-      var output = document.getElementById("radius");
-      output.innerHTML = slider.value; // Display the default slider value
-
-      // Update the current slider value (each time you drag the slider handle)
-      slider.oninput = function() {
-        output.value = this.value;
-      }
-    </script>
   </body>
 </html>
