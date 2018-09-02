426d20514e1fde876b79cd2dc06cdfa9f4bc59f0
JN646 <josh.ginn@hotmail.co.uk>
2018-09-02 15:22:42 +0100

    Fixed Bug: Local/Immediate Function Variables.

--- index.php
+++ index.php
@@ -84,31 +84,31 @@
       <?php
       // Search Button Press
       if (isset($_POST["btnSearch"])) {
-          $longVal = trim($_POST["long"]);
-          $latVal = trim($_POST["lat"]);
-          $radVal1 = trim($_POST["rad1"]);
-          $radVal2 = trim($_POST["rad2"]);
-          $monthVal = trim($_POST["month"]);
-          $yearVal = trim($_POST["year"]);
+          $longVal    = trim($_POST["long"]);
+          $latVal     = trim($_POST["lat"]);
+          $radVal1    = trim($_POST["rad1"]);
+          $radVal2    = trim($_POST["rad2"]);
+          $monthVal   = trim($_POST["month"]);
+          $yearVal    = trim($_POST["year"]);
 
           // Get SQL
           // Precalculation of ranges
-          $latLow1 = $latVal - $radVal1;
-          $latHigh1 = $latVal + $radVal1;
-          $longLow1 = $longVal - $radVal1;
-          $longHigh1 = $longVal + $radVal1;
+          $latLow1    = $latVal - $radVal1;
+          $latHigh1   = $latVal + $radVal1;
+          $longLow1   = $longVal - $radVal1;
+          $longHigh1  = $longVal + $radVal1;
 
-          $latLow2 = $latVal - $radVal2;
-          $latHigh2 = $latVal + $radVal2;
-          $longLow2 = $longVal - $radVal2;
-          $longHigh2 = $longVal + $radVal2;
+          $latLow2    = $latVal - $radVal2;
+          $latHigh2   = $latVal + $radVal2;
+          $longLow2   = $longVal - $radVal2;
+          $longHigh2  = $longVal + $radVal2;
 
           // Start Timer
           $starttime = microtime(true);
 
           // Run Queries
           $resultCount_Immediate = sqlImmediate($mysqli,$longLow1,$longHigh1,$latLow1,$latHigh1,$latVal,$longVal,$radVal1,$monthVal,$yearVal);
-          $resultCount_Local = sqlLocal($mysqli,$longLow1,$longHigh1,$latLow1,$latHigh1,$latVal,$longVal,$radVal1,$monthVal,$yearVal);
+          $resultCount_Local = sqlLocal($mysqli,$longLow2,$longHigh2,$latLow2,$latHigh2,$latVal,$longVal,$radVal2,$monthVal,$yearVal);
 
           // Calculates total time taken
           $duration = microtime(true) - $starttime;
