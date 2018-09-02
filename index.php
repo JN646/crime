527c10f34a725f09245db9761f2462b8bec2c31c
JN646 <josh.ginn@hotmail.co.uk>
2018-09-02 12:32:08 +0100

    Get location error handeling.

--- index.php
+++ index.php
@@ -181,20 +181,41 @@
       </div>
     </div>
     <script>
+      // Get Boxes to put coordinates in.
       var latBoxVal = document.getElementById("latBox");
       var longBoxVal = document.getElementById("longBox");
 
+      // Get location
       function getLocation() {
           if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(showPosition);
+              navigator.geolocation.getCurrentPosition(showPosition,showError);
           } else {
               alert("Geolocation is not supported by this browser.");
           }
       }
 
+      // Put information in lat/Long boxes.
       function showPosition(position) {
           latBoxVal.value = position.coords.latitude;
           longBoxVal.value = position.coords.longitude;
+      }
+
+      // Handle Errors
+      function showError(error) {
+          switch(error.code) {
+              case error.PERMISSION_DENIED:
+                  alert("User denied the request for Geolocation.")
+                  break;
+              case error.POSITION_UNAVAILABLE:
+                  alert("Location information is unavailable.")
+                  break;
+              case error.TIMEOUT:
+                  alert("The request to get user location timed out.")
+                  break;
+              case error.UNKNOWN_ERROR:
+                  alert("An unknown error occurred.")
+                  break;
+          }
       }
     </script>
   </body>
