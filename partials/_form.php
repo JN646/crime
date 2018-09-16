fef8db5d376b879647a8d742950810f507363e0e
JN646 <josh.ginn@hotmail.co.uk>
2018-09-16 01:17:26 +0100

    JavaScript Form Validation.

--- partials/_form.php
+++ partials/_form.php
@@ -1,16 +1,17 @@
 <!-- Search Form -->
 <form class="form dataForm alert-primary" action="output.php" method="post">
+  <p id='notificationSearch' class='alert alert-danger' style='display: none;'>All fields must have a value.</p>
 <div class='row'>
   <!-- Latitude -->
   <div class='col form-group'>
     <label for="lat">Latitude</label>
-    <input id='latBox' class="form-control" size='8' type="text" name="lat" value="52.1367078">
+    <input id='latBox' class="form-control" onkeydown="checkEmpty()" size='8' type="text" name="lat" value="52.1367078">
   </div>
 
   <!-- Longitude -->
   <div class='col form-group'>
     <label for="long">Longitude</label>
-    <input id='longBox' class="form-control" size='8' type="text" name="long" value="-0.4688611">
+    <input id='longBox' class="form-control" onkeydown="checkEmpty()" size='8' type="text" name="long" value="-0.4688611">
   </div>
 
   <!-- Get GPS Locations -->
@@ -22,13 +23,13 @@
   <!-- Immediate Area -->
   <div class='col form-group'>
     <label for="rad1">Immediate</label>
-    <input id='radius' class="form-control" size='3' type="text" name="rad1" value="0.005">
+    <input id='radius' class="form-control" onkeydown="checkEmpty()" size='3' type="text" name="rad1" value="0.005">
   </div>
 
   <!-- Local Area -->
   <div class='col form-group'>
     <label for="rad2">Local</label>
-    <input id='radius2' class="form-control" size='3' type="text" name="rad2" value="0.02">
+    <input id='radius2' class="form-control" onkeydown="checkEmpty()" size='3' type="text" name="rad2" value="0.02">
   </div>
 
   <!-- Month -->
@@ -42,7 +43,33 @@
   <!-- Search Button -->
   <div class='col form-group'>
     <label>&nbsp</label>
-    <button class="form-control btn btn-primary" type="submit" name="btnSearch"><i class="fas fa-search"></i> Search</button>
+    <button id="btnSearch" class="form-control btn btn-primary" type="submit" name="btnSearch"><i class="fas fa-search"></i> Search</button>
   </div>
 </div>
 </form>
+
+<!-- Basic Validation -->
+<script type="text/javascript">
+  // Basic Validation.
+  var notification = document.getElementById('notificationSearch');
+  var btnSearch = document.getElementById('btnSearch');
+  var inputLat = document.getElementById('latBox');
+  var inputLong = document.getElementById('longBox');
+  var inputRadius = document.getElementById('radius');
+  var inputRadius2 = document.getElementById('radius2');
+
+  // Check if empty.
+  function checkEmpty() {
+    if (inputLat.value == "" || inputLong.value == "" || inputRadius.value == "" || inputRadius2.value == "") {
+      // Hide button.
+      btnSearch.style.display = "none";
+      // Show error message.
+      notificationSearch.style.display = "";
+    } else {
+      // Show button.
+      btnSearch.style.display = "";
+      // Hide error message.
+      notificationSearch.style.display = "none";
+    }
+  }
+</script>
