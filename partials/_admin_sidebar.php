47ab3b0fdb9566a46c483a3f7a39a231ff50769b
JN646 <josh.ginn@hotmail.co.uk>
2018-09-28 10:52:17 +0200

    New Sidebar UI.

--- partials/_admin_sidebar.php
+++ partials/_admin_sidebar.php
@@ -1,4 +1,21 @@
-<ul>
-  <li><a href='admin.php'>Home</a></li>
-  <li><a href='serverinfo.php'>Server Info</a></li>
-</ul>
+<!-- Links -->
+<div class="card">
+  <div class="card-header">
+    <h5>Links</h5>
+  </div>
+  <ul class="nav flex-column">
+    <li class="nav-item"><a class="nav-link" href="admin.php"><i class="fas fa-home"></i> Home</a></li>
+  </ul>
+</div>
+<br>
+
+<!-- Config -->
+<div class="card">
+  <div class="card-header">
+    <h5>Config</h5>
+  </div>
+  <ul class="nav flex-column">
+    <li class="nav-item"><a class="nav-link" href="serverinfo.php"><i class="fas fa-server"></i> Server Info</a></li>
+  </ul>
+</div>
+<br>
