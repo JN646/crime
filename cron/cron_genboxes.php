de8d693f96c911ece1efdea6a08b1174b199d174
Darius Alexander <dariusjalexander@gmail.com>
2018-10-09 19:08:46 +0100

    updates

--- cron/cron_genboxes.php
+++ cron/cron_genboxes.php
@@ -3,31 +3,54 @@
 require_once '../config/config.php';
 require_once '../lib/functions.php';
 
+
+//Constants
+$d180p = 180.0/PI();
+$dp180 = PI()/180;
+$eR = 6317000; //earth radius in meters
+$aRHop = $boxHop/$eR; //arc radius (for hop)
+
+echo "starting... <br>";
+echo "hop in meters: " . $boxHop . "<br>";
+echo "rad hop: " . $aRHop . "<br>";
+echo "deg hop: " . radToDeg($aRHop) . "<br>";
+
 // Run Functions
 genBoxes($mysqli, $boxHop);
-destroyBoxes($mysqli, $boxHop, $boxSize);
-prioritiseBoxes($mysqli);
+//destroyBoxes($mysqli, $boxHop, $boxSize);
+//prioritiseBoxes($mysqli);
+
+
+//############## Geometry Conversion Maths ##########################################
+function degToRad($deg) {
+	return $deg * PI()/180;
+}
+
+function radToDeg($rad) {
+	return $rad * 180.0/PI();
+}
 
 //############## GENERATE BOXES ################################################
 function genBoxes($mysqli, $boxHop)
-{
+{	
 	//############## INSERT BOXES ################################################
 	function insertBoxes($mysqli, $ukLatMin, $ukLatMax, $ukLongMin, $ukLongMax, $boxHop) {
+	  global $aRHop;
 	  $x = $ukLatMin;
 	  while($x < $ukLatMax) {
 	    $y = $ukLongMin;
 	    while($y < $ukLongMax) {
 	      $sql = "INSERT INTO `box` (latitude, longitude) VALUES ($x, $y)";
 	      $result = mysqli_query($mysqli, $sql);
-
+		
 	      // If Error.
 	      if (!$result) {
 	          die('<p class="SQLError">Could not insert boxes: ' . mysqli_error($mysqli) . '</p>');
 	      }
 
-	      $y += $boxHop;
+	      $y += radToDeg($aRHop);
 	    }
-	    $x += $boxHop;
+	    $x += radToDeg($aRHop);
 	  }
 	}
 	
@@ -45,39 +68,34 @@ function genBoxes($mysqli, $boxHop)
 }
 
 //############## DESTORY BOXES #################################################
+/*
 function destroyBoxes($mysqli, $boxHop, $boxSize) {
-	// Get Random Lat Long.
+	// Get Random box.
 	$sql = "SELECT * FROM `box` ORDER BY RAND() LIMIT 1";
 	$result = mysqli_query($mysqli, $sql);
 	$row = mysqli_fetch_assoc($result);
-
+	
 	// Threshold
-	$threshold = 0.1;
-
+	$thresh = 0.1;
+	
 	// Assign Variables
 	$longVal = $row['longitude'];
 	$latVal = $row['latitude'];
-
-	// Random Lat Long.
+	
+	// Lat Long of box.
 	echo $latVal . "  " . $longVal . "<br>";
-
-	// Calculate Boundaries
-	$longHigh = $longVal + $threshold;
-	$longLow  = $longVal - $threshold;
-	$latHigh = $latVal + $threshold;
-	$latLow = $latVal - $threshold;
-
-	$searchCrime = "SELECT id, Longitude, Latitude, SQRT(POW(Latitude-'$latVal', 2)+POW(Longitude-'$longVal', 2)) FROM data
-	WHERE Latitude > $latLow AND Latitude < $latHigh AND Longitude > $longLow AND Longitude < $longHigh";
-
+	
+	$searchCrime = "SELECT COUNT(id)=0, Longitude, Latitude FROM data
+	WHERE SQRT(POW(Latitude-'$latVal', 2)+POW(Longitude-'$longVal', 2)) < $thresh";
+	
 	$crimeResult = mysqli_query($mysqli, $searchCrime);
-
+	
 	// If Error.
 	if (!$crimeResult) {
 			die('<p class="SQLError">Could not run query: ' . mysqli_error($mysqli) . '</p>');
 	}
 }
-
+*/
 //############## PRIORITISE BOXES ##############################################
 function prioritiseBoxes($mysqli) {
 	$max = getHighestID($mysqli);
@@ -93,27 +111,27 @@ function calcPriority($mysqli, $id)
 	//given a db and id, calculate a priority value
 	$sql = "SELECT * from `box` WHERE `id` = $id";
 	$result = mysqli_query($mysqli, $sql);
-
+	
 	// If Error
 	if (!$result) {
 			return FALSE;
 	}
-
+	
 	$row = mysqli_fetch_row($result);
-
+	
 	$now = (int)strtotime(mysqli_fetch_row(mysqli_query($mysqli, "SELECT CURTIME()"))[0]);
-
+	
 	if(!is_null($row[4])) {
 		$rowtime = (int)strtotime($row[4]);
 		$p = ($now-$rowtime) * ($row[5]+1); //time difference * requests
 	} else {
 		$p = 999 + $row[5]; //set high priority if no lastupdate
 	}
-
+	
 	$sql = "UPDATE `box` SET priority = $p WHERE id = $id";
 	mysqli_query($mysqli, $sql);
 }
 
 // Header and Return
-header('Location: ' . $_SERVER['HTTP_REFERER']);
+//header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
