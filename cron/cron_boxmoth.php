ec15979bf5c5b5df39ba81e61c3ea5695aa5b42c
Darius Alexander <dariusjalexander@gmail.com>
2018-10-09 11:01:50 +0100

    Update cron/cron_boxmoth.php

--- cron/cron_boxmoth.php
+++ cron/cron_boxmoth.php
@@ -1,18 +1,18 @@
 <?php
-/*
-*	Generates a new row entry in table "box_month" for each month for each box.
-*	Each row contains a number for each type of crime in that box-month.
-*	
-* 	Process:
-*	- [Potential pre-process] calculate priority.
-*	- Select a box (could be random, iterative, or priority based)
-*	- Pull all months for that box
-*	- Decide which months need processing (this could be ones that need re-
-*	  processing, or have not been processed yet. This is why we could do with an
-*	  upload manager.)
-*	- Total the types of crimes inside the box & insert to box_month!
-*	- Repeat indefinately
-*/
+	/*
+	*	Generates a new row entry in table "box_month" for each month for each box.
+	*	Each row contains a number for each type of crime in that box-month.
+	*	
+	* 	Process:
+	*	- [Potential pre-process] calculate priority.
+	*	- Select a box (could be random, iterative, or priority based)
+	*	- Pull all months for that box
+	*	- Decide which months need processing (this could be ones that need re-
+	*	  processing, or have not been processed yet. This is why we could do with an
+	*	  upload manager.)
+	*	- Total the types of crimes inside the box & insert to box_month!
+	*	- Repeat indefinately
+	*/
 
 
 
