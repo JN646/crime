b3e70a7a8b2061793dfab2c7baa5e4e7160b6065
Darius Alexander <dariusjalexander@gmail.com>
2018-10-09 19:21:38 +0100

    fixed

--- config/config.php
+++ config/config.php
@@ -15,11 +15,11 @@ if ($mysqli->connect_error) {
 }
 
 // Global Variables.
-define("LOCAL", "http://localhost:8888/crime/"); //local URL
+define("LOCAL", "http://localhost/crime/"); //local URL
 define("WEB", "http://192.168.1.72:80/crime/"); //website URL
 $environment = LOCAL; //change to WEB if you're live
 
-<<<<<<< HEAD
+
 // IN METERS NOW
 $boxHop = 10000; //10km???
 $boxSize = 120000; //120km
@@ -32,5 +32,4 @@ $TimeSeries_ExecTimer == TRUE;
 
 // Crime Counter
 $CrimeCounter_ExecTimer == TRUE;
->>>>>>> 6d0510ea0fd0460edd4dd281edda6f113d21dde4
 ?>
