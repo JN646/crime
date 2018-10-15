8185e87a52c0361486ea649324f4ac07adaa70e4
Josh <josh.ginn@hotmail.co.uk>
2018-10-15 11:06:11 +0100

    Added Stat Slider.

--- partials/_stats.php
+++ partials/_stats.php
@@ -1,7 +1,6 @@
 <!-- Display Stats -->
 <html>
 <div id='resultStats'>
-  <h3 class=''>Crime</h3>
   <?php
   // Crime Array
   $crime=array(
@@ -29,63 +28,85 @@
   );
   ?>
 
-  <!-- Stat Container -->
-  <div class="statBox">
-    <div class='row'>
-      <?php
-      foreach ($crime as $key=>$value) {
-          ?>
-          <div class='col-md-3 statBox'>
-            <div class="card">
-              <h5 class="card-header text-center"><?php echo $key?></h5>
-              <div class="card-body">
-                <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
-              </div>
-            </div>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="5000">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+      <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <h3>Crimes</h3>
+        <!-- Stat Container -->
+        <div class="statBox">
+          <div class='row'>
+            <?php
+            foreach ($crime as $key=>$value) {
+                ?>
+                <div class='col-md-3 statBox'>
+                  <div class="card">
+                    <h5 class="card-header text-center"><?php echo $key?></h5>
+                    <div class="card-body">
+                      <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
+                    </div>
+                  </div>
+                </div>
+            <?php
+            } ?>
           </div>
-      <?php
-      } ?>
-    </div>
-  </div>
-
-  <h3 class=''>Boxes</h3>
-  <!-- Stat Container -->
-  <div class="statBox">
-    <div class='row'>
-      <?php
-      foreach ($boxes as $key=>$value) {
-          ?>
-          <div class='col-md-3 statBox'>
-            <div class="card">
-              <h5 class="card-header text-center"><?php echo $key?></h5>
-              <div class="card-body">
-                <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
-              </div>
-            </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <h3 class=''>Boxes</h3>
+        <!-- Stat Container -->
+        <div class="statBox">
+          <div class='row'>
+            <?php
+            foreach ($boxes as $key=>$value) {
+                ?>
+                <div class='col-md-3 statBox'>
+                  <div class="card">
+                    <h5 class="card-header text-center"><?php echo $key?></h5>
+                    <div class="card-body">
+                      <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
+                    </div>
+                  </div>
+                </div>
+            <?php
+            } ?>
           </div>
-      <?php
-      } ?>
-    </div>
-  </div>
-
-  <h3 class=''>Other</h3>
-  <!-- Stat Container -->
-  <div class="statBox">
-    <div class='row'>
-      <?php
-      foreach ($other as $key=>$value) {
-          ?>
-          <div class='col-md-3 statBox'>
-            <div class="card">
-              <h5 class="card-header text-center"><?php echo $key?></h5>
-              <div class="card-body">
-                <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
-              </div>
-            </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <h3 class=''>Other</h3>
+        <!-- Stat Container -->
+        <div class="statBox">
+          <div class='row'>
+            <?php
+            foreach ($other as $key=>$value) {
+                ?>
+                <div class='col-md-3 statBox'>
+                  <div class="card">
+                    <h5 class="card-header text-center"><?php echo $key?></h5>
+                    <div class="card-body">
+                      <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
+                    </div>
+                  </div>
+                </div>
+            <?php
+            } ?>
           </div>
-      <?php
-      } ?>
+        </div>
+      </div>
     </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" style='color: black;' aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 
 </div>
