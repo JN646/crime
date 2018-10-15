e39e75a3f2b98f70e99526e22180759170c49de2
Josh <josh.ginn@hotmail.co.uk>
2018-10-15 13:35:37 +0100

    Partial working pagination for User Manager.

--- admin/usermanager.php
+++ admin/usermanager.php
@@ -114,6 +114,39 @@ include('lib/userserver.php');
                 </tr>
               </thead>
               <tbody>
+                <?php
+                $r = mysqli_fetch_row($result);
+                $numrows = $r[0];
+
+                // number of rows to show per page
+                $rowsperpage = 20;
+                // find out total pages
+                $totalpages = ceil($numrows / $rowsperpage);
+
+                // get the current page or set a default
+                if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+                   // cast var as int
+                   $currentpage = (int) $_GET['currentpage'];
+                } else {
+                   // default page num
+                   $currentpage = 1;
+                } // end if
+
+                // if current page is greater than total pages...
+                if ($currentpage > $totalpages) {
+                   // set current page to last page
+                   $currentpage = $totalpages;
+                } // end if
+                // if current page is less than first page...
+                if ($currentpage < 1) {
+                   // set current page to first page
+                   $currentpage = 1;
+                } // end if
+
+                // the offset of the list, based on current page
+                $offset = ($currentpage - 1) * $rowsperpage;
+                ?>
+
                 <?php while ($row = mysqli_fetch_array($results)) {
                           ?>
                   <tr>
@@ -128,7 +161,51 @@ include('lib/userserver.php');
                     </td>
                   </tr>
                 <?php
-                      } ?>
+                      }
+
+                      /******  build the pagination links ******/
+                      echo '<nav aria-label="Page navigation"><ul class="pagination pagination-sm justify-content-center">';
+                      // range of num links to show
+                      $range = 3;
+
+                      // if not on page 1, don't show back links
+                      if ($currentpage > 1) {
+                         // show << link to go back to page 1
+                         echo " <li class='page-item'><a class='page-link' href='{$_SERVER['PHP_SELF']}?currentpage=1'><<</a></li> ";
+                         // get previous page num
+                         $prevpage = $currentpage - 1;
+                         // show < link to go back to 1 page
+                         echo " <li class='page-item'><a class='page-link' href='{$_SERVER['PHP_SELF']}?currentpage=$prevpage'><</a></li> ";
+                      } // end if
+
+                      // loop to show links to range of pages around current page
+                      for ($x = ($currentpage - $range); $x < (($currentpage + $range) + 1); $x++) {
+                         // if it's a valid page number...
+                         if (($x > 0) && ($x <= $totalpages)) {
+                            // if we're on current page...
+                            if ($x == $currentpage) {
+                               // 'highlight' it but don't make a link
+                               echo " <li class='page-item active'><span class='page-link'> $x </span></li> ";
+                            // if not current page...
+                            } else {
+                               // make it a link
+                               echo " <li class='page-item'><a class='page-link' href='{$_SERVER['PHP_SELF']}?currentpage=$x'>$x</a></li> ";
+                            } // end else
+                         } // end if
+                      } // end for
+
+                      // if not on last page, show forward and last page links
+                      if ($currentpage != $totalpages) {
+                         // get next page
+                         $nextpage = $currentpage + 1;
+                          // echo forward link for next page
+                         echo " <li class='page-item'><a class='page-link' href='{$_SERVER['PHP_SELF']}?currentpage=$nextpage'>></a></li> ";
+                         // echo forward link for lastpage
+                         echo " <li class='page-item'><a class='page-link' href='{$_SERVER['PHP_SELF']}?currentpage=$totalpages'>>></a></li> ";
+                      } // end if
+                      /****** end build pagination links ******/
+                      echo "</ul></nav>";
+                      ?>
               </tbody>
             </table>
           </div>
