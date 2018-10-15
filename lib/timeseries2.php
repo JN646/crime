e253bb7f977f7d4aaac301b51530dc0fc7683928
Josh <josh.ginn@hotmail.co.uk>
2018-10-15 10:55:38 +0100

    Stat redesign.

--- lib/timeseries2.php
+++ lib/timeseries2.php
@@ -1,30 +1,28 @@
 <?php
 	require_once '../config/config.php';
-	
-	
+
+
 	$now = "'".date("Y-m")."'";
 	// ($mysqli, [box-id], [month-start], [month-end])
 	$data = getTimeSeries($mysqli, 3391, NULL, $now);
-	
-	
 ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 	<head>
-		<meta charset="utf-8"/>
-		<title>Time Series</title>
 		<script src="../js/Chart.min.js" charset="utf-8"></script>
 	</head>
 	<body>
-		
+
+		<p>Hello</p>
 		<canvas id="lineChart"></canvas>
-		
+
 		<script type="text/javascript">
 			// Get array from PHP
 			var myData = <?php echo json_encode($data); ?>;
-			
+
 			console.log(myData);
-			
+
 			var ctxL = document.getElementById("lineChart").getContext('2d');
 			var lineChart = new Chart(ctxL, {
 				type: 'line',
@@ -37,62 +35,62 @@
 							fill: false,
 							borderColor: 'rgba(255, 0, 0, 0.5)'
 						}, {
-							label: myData.datasets.label[1], 
+							label: myData.datasets.label[1],
 							data: myData.datasets.data['Burglary'],
 							fill: false,
 							borderColor: 'rgba(0, 255, 0, 0.5)'
 						}, {
-							label: myData.datasets.label[2], 
+							label: myData.datasets.label[2],
 							data: myData.datasets.data['Other theft'],
 							fill: false,
 							borderColor: 'rgba(0, 0, 255, 0.5)'
 						}, {
-							label: myData.datasets.label[3], 
+							label: myData.datasets.label[3],
 							data: myData.datasets.data['Public order'],
 							fill: false,
 							borderColor: 'rgba(255, 255, 0, 0.5)'
 						}, {
-							label: myData.datasets.label[4], 
+							label: myData.datasets.label[4],
 							data: myData.datasets.data['Violence and sexual offences'],
 							fill: false,
 							borderColor: 'rgba(255, 0, 255, 0.5)'
 						}, {
-							label: myData.datasets.label[5], 
+							label: myData.datasets.label[5],
 							data: myData.datasets.data['Vehicle crime'],
 							fill: false,
 							borderColor: 'rgba(0, 255, 255, 0.5)'
 						}, {
-							label: myData.datasets.label[6], 
+							label: myData.datasets.label[6],
 							data: myData.datasets.data['Criminal damage and arson'],
 							fill: false,
 							borderColor: 'rgba(0, 0, 0, 0.5)'
 						}, {
-							label: myData.datasets.label[7], 
+							label: myData.datasets.label[7],
 							data: myData.datasets.data['Other crime'],
 							fill: false,
 							borderColor: 'rgba(255, 0, 0, 0.5)'
 						}, {
-							label: myData.datasets.label[8], 
+							label: myData.datasets.label[8],
 							data: myData.datasets.data['Robbery'],
 							fill: false,
 							borderColor: 'rgba(0, 255, 0, 0.5)'
 						}, {
-							label: myData.datasets.label[9], 
+							label: myData.datasets.label[9],
 							data: myData.datasets.data['Bicycle theft'],
 							fill: false,
 							borderColor: 'rgba(0, 0, 255, 0.5)'
 						}, {
-							label: myData.datasets.label[10], 
+							label: myData.datasets.label[10],
 							data: myData.datasets.data['Drugs'],
 							fill: false,
 							borderColor: 'rgba(255, 255, 0, 0.5)'
 						}, {
-							label: myData.datasets.label[11], 
+							label: myData.datasets.label[11],
 							data: myData.datasets.data['Shoplifting'],
 							fill: false,
 							borderColor: 'rgba(255, 0, 255, 0.5)'
 						}, {
-							label: myData.datasets.label[12], 
+							label: myData.datasets.label[12],
 							data: myData.datasets.data['Theft from the person'],
 							fill: false,
 							borderColor: 'rgba(0, 255, 255, 0.5)'
@@ -102,7 +100,7 @@
 			});
 		</script>
 	</body>
-	
+
 	<?php
 		function getTimeSeries($mysqli, $bID, $mStart = NULL, $mEnd = NULL)
 		{
@@ -112,7 +110,7 @@
 				echo "Start date cannot be after or the same as end date<br>";
 				return 0;
 			}
-			
+
 			// Returns the boxmonths for a given box ID, and +1 to requests
 			$out = [ 'labels'=>[], 'datasets'=>[
 					'label'=>[
@@ -133,11 +131,11 @@
 					], 'data'=>[]
 				]
 			];
-			
+
 			// Add 1 to Requests
 			$addQ = "UPDATE `box` SET `requests` = `requests` + 1 WHERE `id` = $bID";
 			$addR = mysqli_query($mysqli, $addQ);
-			
+
 			// Build a Smart Query
 			$TSQ = "SELECT * FROM `box_month` WHERE `bm_boxid` = $bID";
 			if(!is_null($mStart)) {
@@ -146,10 +144,10 @@
 			if(!is_null($mEnd)) {
 				$TSQ = $TSQ." AND `bm_month` <= $mEnd";
 			}
-			
+
 			// Return Time Series
 			$TSR = mysqli_query($mysqli, $TSQ);
-			
+
 			while($row = mysqli_fetch_assoc($TSR)) {
 				$out['labels'][] = $row['bm_month'];
 				//echo $row['bm_month']."<br>";
@@ -162,9 +160,3 @@
 		}
 	?>
 </html>
-
-
-
-
-
-
--- partials/_stats.php
+++ partials/_stats.php
@@ -2,26 +2,44 @@
 <html>
 <div id='resultStats'>
   <h3 class=''>Crime</h3>
+  <?php
+  // Crime Array
+  $crime=array(
+      'Crimes'=>callStat($mysqli, "'nCrimes'"),
+      'No Locations'=>callStat($mysqli, "'nCrimesNoLoc'"),
+      'Crime Types'=>callStat($mysqli, "'nCrimeTypes'"),
+      'Months'=>callStat($mysqli, "'nMonths'"),
+      //'Falls Within'=>callStat($mysqli, "'FallsWithin'"), // this is a shit stat
+      'Reporting Constabularies'=>callStat($mysqli, "'ReportedBy'")
+  );
+
+  // Box Array
+  $boxes=array(
+      'Boxes'=>callStat($mysqli, "'nBoxes'"),
+      'Active Boxes'=>callStat($mysqli, "'nBoxesActive'"),
+      'NULL Boxes'=>callStat($mysqli, "'nBoxesNull'"),
+      'Box Months'=>callStat($mysqli, "'nBoxmonths'")
+  );
+
+  // Other Array
+  $other=array(
+      'Users'=>callStat($mysqli, "'nUsers'"),
+      'Stats (inclusive)'=>callStat($mysqli, "'nStats'"),
+      'Stats Last Update'=>callStat($mysqli, "'statsLastUpdate'")
+  );
+  ?>
+
   <!-- Stat Container -->
   <div class="statBox">
     <div class='row'>
       <?php
-      $crime=array(
-          'Crimes'=>callStat($mysqli, "'nCrimes'"),
-          'No Locations'=>callStat($mysqli, "'nCrimesNoLoc'"),
-          'Crime Types'=>callStat($mysqli, "'nCrimeTypes'"),
-          'Months'=>callStat($mysqli, "'nMonths'"),
-          //'Falls Within'=>callStat($mysqli, "'FallsWithin'"), // this is a shit stat
-          'Reporting Constabularies'=>callStat($mysqli, "'ReportedBy'")
-      );
-
       foreach ($crime as $key=>$value) {
           ?>
           <div class='col-md-3 statBox'>
             <div class="card">
+              <h5 class="card-header text-center"><?php echo $key?></h5>
               <div class="card-body">
-                <h5 class="card-title text-center"><?php echo $key?></h5>
-                <h6 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h6>
+                <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
               </div>
             </div>
           </div>
@@ -29,27 +47,19 @@
       } ?>
     </div>
   </div>
-
 
   <h3 class=''>Boxes</h3>
   <!-- Stat Container -->
   <div class="statBox">
     <div class='row'>
       <?php
-      $boxes=array(
-          'Boxes'=>callStat($mysqli, "'nBoxes'"),
-          'Active Boxes'=>callStat($mysqli, "'nBoxesActive'"),
-          'NULL Boxes'=>callStat($mysqli, "'nBoxesNull'"),
-          'Box Months'=>callStat($mysqli, "'nBoxmonths'")
-      );
-
       foreach ($boxes as $key=>$value) {
           ?>
           <div class='col-md-3 statBox'>
             <div class="card">
+              <h5 class="card-header text-center"><?php echo $key?></h5>
               <div class="card-body">
-                <h5 class="card-title text-center"><?php echo $key?></h5>
-                <h6 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h6>
+                <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
               </div>
             </div>
           </div>
@@ -58,25 +68,18 @@
     </div>
   </div>
 
-
   <h3 class=''>Other</h3>
   <!-- Stat Container -->
   <div class="statBox">
     <div class='row'>
       <?php
-      $other=array(
-          'Users'=>callStat($mysqli, "'nUsers'"),
-          'Stats (inclusive)'=>callStat($mysqli, "'nStats'"),
-          'Stats Last Update'=>callStat($mysqli, "'statsLastUpdate'")
-      );
-
       foreach ($other as $key=>$value) {
           ?>
           <div class='col-md-3 statBox'>
             <div class="card">
+              <h5 class="card-header text-center"><?php echo $key?></h5>
               <div class="card-body">
-                <h5 class="card-title text-center"><?php echo $key?></h5>
-                <h6 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h6>
+                <h3 class="card-subtitle text-center mb-2 text-muted"><?php echo $value?></h3>
               </div>
             </div>
           </div>
